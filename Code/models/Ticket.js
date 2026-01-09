2607ebef39d6ae596f3401a91618c27a65b65a00
Jamira Dürsteler <Jamira.duersteler@tbz.edu.ch>
2026-01-09 09:57:43 +0100

    Created ticket model

---
+++ Code/models/Ticket.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+ 
+const TicketSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      enum: ['Open', 'In Progress', 'Review', 'Done'],
+      required: [true, 'Status ist erforderlich'],
+      default: 'Open',
+    },
+    titel: {
+      type: String,
+      required: [true, 'Titel ist erforderlich'],
+      trim: true,
+    },
+    text: {
+      type: String,
+      required: [true, 'Text ist erforderlich'],
+      trim: true,
+    },
+    reviewDatum: {
+      type: Date,
+      default: null,
+    },
+    doneDatum: {
+      type: Date,
+      default: null,
+    },
+    mitarbeiterId: {
+      type: String,
+      required: [true, 'Mitarbeiter-Zuweisung ist erforderlich'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+ 
+module.exports = mongoose.model('Ticket', TicketSchema);
